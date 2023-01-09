eb6e0f4dfb0f15a1ab3c50017c894a5340687b50
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-09 13:37:21 -0600

    added header.js and main.js.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from './Header.js';
+import Main from './Main.js';
+
+
+class App extends React.Component {
+     
+
+
+
+
+  render(){
+    return (
+      //add components
+      <> 
+      <Header />
+      <Main />
+      <footer>Author Susan Hargus</footer>
+      </>
+    )
+  }
+
+} 
+export default App; 
