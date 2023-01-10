d52fcb3ed7a9524091ec2857e0b309cbc8215378
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-10 09:19:31 -0600

    added hornedBeast file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header.js';
-import Main from './Main.js';
+// import Main from './Main.js';
 
 
 class App extends React.Component {
