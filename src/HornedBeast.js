7911c29afd2065663310908fdfca1647e091905d
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-11 14:18:01 -0600

    added buttons

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './HornedBeast.css';
+import Button from 'react-bootstrap/Button';
 
 
 
@@ -10,24 +11,41 @@ class HornedBeast extends React.Component {
     this.state = {
       // count likes
       likes: 0,
+
+      // hbNeeded = () => {
+      //   this.setState({
+      //     hbNeeded: true,
+      //   });
+      // }
+
+      // hbGot = () => {
+      //   this.setState({
+      //     hbNeeded: false,
+      //   });
+      // }
     }
   }
   handleLikes = () => {
-this.setState({
-  likes:this.state.likes + 1,
-})
+    this.setState({
+      likes: this.state.likes + 1,
+    })
   }
 
   render() {
     console.log('props?', this.props);
     return (
       <>
+      <article>
         <h2>{this.props.title}</h2>
         <img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
         <p>{this.props.description}</p>
         <p>{this.state.likes}Likes!</p>
         <p onClick={this.handleLikes}>Click to like this Beast</p>
 
+       
+        <Button onClick={this.hbNeeded} className="buttonMargin"></Button>
+        <Button variant="success"></Button>
+        </article>
       </>
     )
 
