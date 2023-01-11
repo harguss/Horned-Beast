6a9064b6306829439c7e6255abefa5753397bdb6
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-11 13:23:13 -0600

    added likes and main.css and hb.css

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,8 +1,22 @@
 import React from "react";
+import './HornedBeast.css';
+import './Main.css';
 
 
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // count likes
+      likes: 0,
+    }
+  }
+  handleLikes = () => {
+this.setState({
+  likes:this.state.likes + 1,
+})
+  }
 
   render() {
     console.log('props?', this.props);
@@ -11,6 +25,9 @@ class HornedBeast extends React.Component {
         <h2>{this.props.title}</h2>
         <img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
         <p>{this.props.description}</p>
+        <p>{this.state.likes}Likes!</p>
+        <p onClick={this.handleLikes}>Click to like this Beast</p>
+
       </>
     )
 
