181d5dc02ef18ac46801b89ba3e367bffb41da56
SHargus <87950738+harguss@users.noreply.github.com>
2023-01-16 12:34:39 -0600

    modal added and showModal working

--- src/App.js
+++ src/App.js
@@ -6,28 +6,39 @@ import Footer from './Footer';
 import Container from 'react-bootstrap/Container';
 import data from './data.json';
 // import SelectedBeast from './SelectBeast'
-// import Modal from 'react-bootstrap/Modal';
+import Modal from 'react-bootstrap/Modal';
+// import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
-  // constructor(props){
-  //   // super(props);
-  //   this.state ={
-  //     // showModal:false,
-  //     // selectBeast: '',
+  constructor(props){
+     super(props);
+     this.state ={
+       showModal:false,
+       selectBeast: '',
 
-  //   }
-  // }
+    }
+  }
   // helper functions
-
+handleOnShow = () => {
+  this.setState({
+    showModal:true,
+  });
+}
   render() {
     return (
       <>
       <Container >
+        <p onClick={this.handleOnShow}>SHow me now est modal</p>
         <Header />
         <Main
          data ={data}/>
-        {/* <SelectedBeast/> */}
-        <Footer />
+        <footer>Susan Hargus</footer>
+        {/* <SelectedBeast /> */}
+        <Modal show={this.state.showModal} onHide={this.handleClose}>
+        {/* <Modal.Header closeButton>
+          <Modal.Title>{this.state.selectBeast}</Modal.Title>
+        </Modal.Header> */}
+      </Modal>
       </Container>
       </>
     );
